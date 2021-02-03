bc138d65858112f6797c488ff6c561ed2541d9d0
Admin <glam3@uw.edu>
2021-02-02 23:11:34 -0800

    First dotile setup

---
+++ dotsetup.sh
@@ -0,0 +1,12 @@
+# Bash profile
+ln -fs ~/dotfiles/.bashrc ~/.bashrc
+#Git
+ln -fs ~/dotfiles/.gitconfig ~/.gitconfig
+#Vim
+ln -fs ~/dotfiles/.vimrc ~/.vimrc
+#Folder problems? Has inner git?
+ln -fs ~/dotfiles/.vim ~/.vim
+#SSH
+ln -fs ~/dotfiles/./ssh/config ~/.ssh/config
+#tmux
+ln -fs ~/dotfiles/.tmux.conf ~/.tmux.conf 
